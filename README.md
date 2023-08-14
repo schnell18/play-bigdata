5b3ed126e660dc4c4920c378c8e56730a981c561
Justin Zhang <schnell18@gmail.com>
2023-08-14 19:44:30 +1200

    Add sample program to extract text from PDF/word

--- README.md
+++ README.md
@@ -4,10 +4,11 @@ This is a collection mini projects to explore Big Data.
 
 ## Catelog
 
-| sub-directory      | comment                             |
-| ------------------ | ----------------------------------- |
-| hadoop             | basic HDFS and map/reduce stuff     |
-| hbase              | columnar database on top of hadoop  |
-| hive               | dataware w/ SQL interface           |
-| hive-jdbc          | hive JDBC access demonstration      |
-| spark              | in-memory big data analytics engine |
+| sub-directory      | comment                                       |
+| ------------------ | --------------------------------------------- |
+| hadoop             | basic HDFS and map/reduce stuff               |
+| hbase              | columnar database on top of hadoop            |
+| hive               | dataware w/ SQL interface                     |
+| hive-jdbc          | hive JDBC access demonstration                |
+| spark              | in-memory big data analytics engine           |
+| elasticsearch      | extract text from document and ingest into ES |
---
+++ elasticsearch/src/main/java/org/tinker/big/data/unstructured/document/AutoParserTest.java
@@ -0,0 +1,44 @@
+package org.tinker.big.data.unstructured.document;
+
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.sax.BodyContentHandler;
+import org.xml.sax.SAXException;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+
+public class AutoParserTest {
+
+    public static void main(String[] args) {
+        String docFile = "report.pdf";
+        if (args != null && args.length >= 1) {
+            docFile = args[0];
+        }
+        File file = new File(docFile);
+        try {
+            String content = autoParse(file);
+            System.out.println(content);
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static String autoParse(File file) throws IOException, SAXException, TikaException {
+        AutoDetectParser parser = new AutoDetectParser();
+        BodyContentHandler handler = new BodyContentHandler();
+        Metadata metadata = new Metadata();
+
+        try (InputStream stream = Files.newInputStream(file.toPath())) {
+            parser.parse(stream, handler, metadata);
+            for (String name: metadata.names() ) {
+                System.out.println(name + ":\t" + metadata.get(name));
+            }
+            return handler.toString();
+        }
+    }
+}
